6080e700cd0ddc903061db3f9e999bf13a8d8378
Davo Smith <git@davosmith.co.uk>
2015-09-07 21:57:31 +0100

    Add missing 'component' to version.php

--- version.php
+++ version.php
@@ -29,4 +29,5 @@ $plugin->cron     = 0;
 $plugin->requires = 2010112400; // Moodle 2.0+
 $plugin->release  = '2.x (Build: 2015080500)';
 $plugin->maturity = MATURITY_STABLE;
+$plugin->component = 'gradeexport_checklist';
 $plugin->dependencies = array('mod_checklist' => 2010041800); // Must have checklist activity module installed.
